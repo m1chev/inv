7bd966e25142456a063ecbf023a50b6f0efba01b
Aleksandar Karamfilov <karamfilovs@gmail.com>
2018-03-11 12:39:36 +0200

    Added multiple browser support

--- src/test/java/inv/InvStepDefinitions.java
+++ src/test/java/inv/InvStepDefinitions.java
@@ -1,5 +1,6 @@
 package inv;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
@@ -10,31 +11,84 @@ import inv.pages.ClientPage;
 import inv.pages.DashboardPage;
 import inv.pages.ItemPage;
 import inv.pages.LoginPage;
+import org.apache.commons.io.FileUtils;
 import org.fest.assertions.Assertions;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import util.Constants;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class InvStepDefinitions {
-    LoginPage loginPage;
-    DashboardPage dashboardPage;
-    ItemPage itemPage;
-    ClientPage clientPage;
+    private static final Logger LOGGER = LoggerFactory.getLogger(InvStepDefinitions.class);
+    //Drivers location
+    private static final String chromeDriverLocation = "C:\\webdrivers\\chromedriver.exe";
+    private static final String firefoxDriverLocation = "C:\\webdrivers\\geckodriver.exe";
+    private static final String ieDriverLocation = "C:\\webdrivers\\IEDriverServer.exe";
+    private static final String chromeProperty = "webdriver.chrome.driver";
+    private static final String firefoxProperty = "webdriver.gecko.driver";
+    private static final String ieProperty = "webdriver.ie.driver";
+    //Page objects
+    private LoginPage loginPage;
+    private DashboardPage dashboardPage;
+    private ItemPage itemPage;
+    private ClientPage clientPage;
     WebDriver driver;
 
-    @Before
-    public void before() {
-        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    private void startBrowser(String browser) {
+        if (browser.equalsIgnoreCase("firefox")) {
+            System.setProperty(firefoxProperty, firefoxDriverLocation);
+            driver = new FirefoxDriver();
+            configureBrowser(browser);
+        }
+        if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty(chromeProperty, chromeDriverLocation);
+            driver = new ChromeDriver();
+            configureBrowser(browser);
+        }
+        if (browser.equalsIgnoreCase("ie")) {
+            System.setProperty(ieProperty, ieDriverLocation);
+            driver = new InternetExplorerDriver();
+            configureBrowser(browser);
+        }
 
     }
 
+    private void configureBrowser(String browser) {
+        LOGGER.info("Starting browser:" + browser);
+        driver.manage().deleteAllCookies(); //delete cookies
+        driver.manage().window().maximize(); //To maximize browser
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);   //Implicit wait
+    }
+
+
+    @Before
+    public void before() {
+        startBrowser("firefox");
+    }
+
     @After
-    public void after() {
+    public void after(Scenario scenario) {
+        if (scenario.isFailed()) {
+            try {
+                TakesScreenshot screenshot = (TakesScreenshot) driver;
+                File src = screenshot.getScreenshotAs(OutputType.FILE); //capture screenshot
+                String fileName = scenario.getName() + ".png";
+                FileUtils.copyFile(src, new File("E:\\screenshots\\" + fileName)); //copy file to location
+                LOGGER.info("Successfully captured a screenshot");
+                LOGGER.info("Stored image:" + fileName + " at:" + "E:\\screenshots\\");
+            } catch (Exception e) {
+                LOGGER.info("Exception while taking screenshot " + e.getMessage());
+            }
+        }
         driver.quit();
     }
 
@@ -89,7 +143,7 @@ public class InvStepDefinitions {
 
     @When("^I create new client with name:\"(.*)\" and vat:\"(.*)\" and address:\"(.*)\" and town:\"(.*)\"$")
     public void createNewClient(String name, String vat, String address, String town) {
-       clientPage.createClient(name, vat, address, town);
+        clientPage.createClient(name, vat, address, town);
     }
 
     @And("^I press Login button$")
@@ -107,12 +161,12 @@ public class InvStepDefinitions {
         clientPage.deleteAllClients();
     }
 
-    @Then("^login error message with text should be displayed:\"([^\"]*)\"$")
+    @Then("^login error message with text should be displayed:\"(.*)\"$")
     public void loginErrorMessage(String errorMessage) {
         loginPage.verifyMessage(errorMessage);
     }
 
-    @Then("^item message with text should be displayed:\"([^\"]*)\"$")
+    @Then("^item message with text should be displayed:\"(.*)\"$")
     public void itemSuccessMessage(String successMessage) {
         Assertions.assertThat(itemPage.getSuccessAddMessage()).as("Item Added").contains(successMessage);
     }
